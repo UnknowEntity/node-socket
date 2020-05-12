69b01e4e03357604981f6b16df7aa0a06996f09d
UnknowEntity <theplayer357@gmail.com>
2020-05-12 09:54:28 +0700

    start

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var client = require("socket.io-client");
+const axios = require("axios");
+
+const PORT = 3000 | 3001 | 3002 | 3003;
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var socketNode = [];
+var socket = client(`http://localhost:${PORT}`);
+socketNode.push(socket);
+
+app.get("/nodes", (req, res) => {
+  const { callback, port } = req.query;
+  const node = `http://localhost:${port}`;
+  const socketNode = client(node);
+  socketNode.push(socketNode);
+  if (callback === "true") {
+    console.info(`Added node ${node} back`);
+    res.json({ status: "Added node Back" }).end();
+  } else {
+    axios.get(`${node}/nodes?callback=true&port=${PORT}`);
+    console.info(`Added node ${node}`);
+    res.json({ status: "Added node" }).end();
+  }
+});
+
+app.get("/hello", (req, res) => {
+  socket.emit("hello");
+});
+
+io.on("connection", (socket) => {
+  console.info(`Socket connected, ID: ${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected, ID: ${socket.id}`);
+  });
+});
+
+socket.on("hello", () => {
+  console.log("hi");
+});
+
+var server = http.listen(PORT, () => {
+  console.log(`Express server running on ${PORT}...`);
+});

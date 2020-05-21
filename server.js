ac7e1aed754abfbf05cd2d81b9d41194be5a6d91
UnknowEntity <theplayer357@gmail.com>
2020-05-22 01:55:21 +0700

    change connecting protocol

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var bodyParser = require("body-parser");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var ws = require("ws");
 var client = require("socket.io-client");
 const axios = require("axios");
 
 const PORT = 3000;
-const key = `node-${Date.now()}`;
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -19,12 +17,16 @@ var socketListeners = (socket) => {
     console.log("Hello");
   });
 
-  socket.on("get nodeList", () => console.log(socket.id));
+  socket.on("get nodeList", () => console.log(nodeList));
 
   socket.on("connect confirm", (data) => {
     console.log("connect confirm");
     keyList.push(data.key);
-    socket.emit("callback confirm", { key, node, length: keyList.length });
+    socket.emit("callback confirm", {
+      key,
+      node: data.node,
+      length: keyList.length,
+    });
     if (data.length === 1 && keyList.length > 2) {
       io.emit("request join net", { node: data.node, key: data.key });
     }
@@ -51,34 +53,45 @@ var socketListeners = (socket) => {
   return socket;
 };
 
-var socketNode = [];
-var keyList = [key];
+var socketNodes = [];
+var nodeList = [];
 socketNode.push(socketListeners(client(`http://localhost:${PORT}`)));
 
-var getSocket = (node) => {
-  var socket = client(node);
-  socketNode.push(socket);
-  socket.emit("connect confirm", { key, node, length: keyList.length });
-  socketListeners(socket);
-};
-
 io.on("connection", (socket) => {
-  socketNode.push(socket);
-  socketListeners(socket);
+  console.info(`Socket connected, ID: ${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected, ID: ${socket.id}`);
+  });
 });
 
 // var socket = socketListener(client(`http://localhost:${PORT}`));
 // socketNode.push(socket);
 
-app.get("/nodes", (req, res) => {
-  const { callback, port, nodeLength } = req.query;
-  const node = `http://localhost:${port}`;
-  getSocket(node);
+app.post("/nodes", (req, res) => {
+  const { host, port } = req.body;
+  const { callback, nodeLength } = req.query;
+  const node = `http://${host}:${port}`;
+  nodeList.push(node);
+  const socketNode = socketListeners(client(node));
+  socketNodes.push(socketNode);
   if (callback === "true") {
+    if (parseInt(nodeLength) > 1 && nodeList.length === 1) {
+      axios.post(`${node}/request-list`, {
+        host: req.hostname,
+        port: PORT,
+      });
+    } else if (nodeList.length > 1 && parseInt(nodeLength) === 1) {
+      axios.post(`${node}/update-list`, {
+        requestNodeList: nodeList,
+      });
+    }
     console.info(`Added node ${node} back`);
     res.json({ status: "Added node Back" }).end();
   } else {
-    axios.get(`${node}/nodes?callback=true&port=${PORT}&nodeLength=${keyList}`);
+    axios.post(`${node}/nodes?callback=true&nodeLength=${nodeList.length}`, {
+      host: req.hostname,
+      port: PORT,
+    });
     console.info(`Added node ${node}`);
     res.json({ status: "Added node" }).end();
   }
@@ -86,11 +99,57 @@ app.get("/nodes", (req, res) => {
 
 app.get("/hello", (req, res) => {
   io.emit("hello");
+  res.json({ status: 200 });
 });
 
 app.get("/node-list", (req, res) => {
   io.emit("get nodeList");
   res.json({ status: 200 });
+});
+
+app.post("/request-list", (req, res) => {
+  const { host, port } = req.body;
+  const node = `http://${host}:${port}`;
+  axios.post(`${node}/update-list`, {
+    requestNodeList: nodeList,
+  });
+  res.json({ status: "request accepted" }).end();
+});
+
+app.post("/update-list", (req, res) => {
+  const { requestNodeList } = req.body;
+  const currentNode = `http://${req.hostname}:${PORT}`;
+  console.log(currentNode);
+
+  for (let index = 0; index < requestNodeList.length; index++) {
+    if (requestNodeList[index] !== currentNode) {
+      axios.post(`${requestNodeList[index]}/request-join`, {
+        host: req.hostname,
+        port: PORT,
+      });
+    }
+  }
+  res.json({ status: "node list return" }).end();
+});
+
+app.post("/request-join", (req, res) => {
+  const { host, port } = req.body;
+  const { callback } = req.query;
+  const node = `http://${host}:${port}`;
+  nodeList.push(node);
+  const socketNode = socketListeners(client(node));
+  socketNodes.push(socketNode);
+  if (callback === "true") {
+    console.info(`Added node ${node} back`);
+    res.json({ status: "Added node Back" }).end();
+  } else {
+    axios.post(`${node}/request-join?callback=true`, {
+      host: req.hostname,
+      port: PORT,
+    });
+    console.info(`Added node ${node}`);
+    res.json({ status: "Added node" }).end();
+  }
 });
 
 var server = http.listen(PORT, () => {
